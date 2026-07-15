fa811656896b25e42de5703932d53528d390e763
ancung <ancungaulia@gmail.com>
2026-07-15 20:15:52 +0700

    fix(frontend): prepare deploy build and favicon

--- landing-page/next.config.ts
+++ landing-page/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["tendentiously-impalpable-dede.ngrok-free.dev"],
 };
 
 export default nextConfig;
